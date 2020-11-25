fc2fa4907cf03a54593d3f9f8d3255e999da5521
zhangqi <15248448516@163.com>
2020-11-25 16:27:11 +0800

    qzui@0.3.22

--- packages/utils/prototype.js
+++ packages/utils/prototype.js
@@ -12,4 +12,34 @@ export default function() {
     const [first, ...os] = this.split('.').reverse()
     return os.reduce((o, k) => ({ [k]: o }), { [first]: value })
   }
+
+  Date.prototype.format = function(fmt) {
+    const o = {
+      'M+': this.getMonth() + 1, // 月份
+      'd+': this.getDate(), // 日
+      'h+': this.getHours(), // 小时
+      'm+': this.getMinutes(), // 分
+      's+': this.getSeconds(), // 秒
+      'q+': Math.floor((this.getMonth() + 3) / 3), // 季度
+      S: this.getMilliseconds() // 毫秒
+    }
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length))
+    for (const k in o)
+      if (new RegExp('(' + k + ')').test(fmt))
+        fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length))
+    return fmt
+  }
+
+  Date.prototype.toTypeStr = function(timestamp) {
+    const time = (timestamp || new Date().getTime()) - this.getTime()
+    return time < 60 * 1000
+      ? '刚刚'
+      : time < 60 * 60 * 1000
+      ? Math.floor(time / (60 * 1000)) + '分钟前'
+      : time < 24 * 60 * 60 * 1000
+      ? Math.floor(time / (60 * 60 * 1000)) + '小时前'
+      : time < 48 * 60 * 60 * 1000
+      ? '昨天'
+      : this.format('yyyy-MM-dd hh:mm')
+  }
 }
